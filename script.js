3be1ffa3cd9d4b73ae17dfebc0992375418de900
MCurb <marcoscurbeco03@gmail.com>
2025-05-06 17:41:53 -0600

    Make the event listener to return a random color
    The on mouse enter event listener now will return a random color every
    time, instead of just black

--- script.js
+++ script.js
@@ -1,29 +1,44 @@
-const containerDiv = document.querySelector(".container")
+const containerDiv = document.querySelector(".container");
 
 function createGrids() {
-     //Create a lot of divs that change their height and width to fit the container perfectly, no matter how much of them
-     let row = 80;
-     let column = 80;
-    for (let i = 0; i < column; i++) {
-        const columnDiv = document.createElement("div")
-        // columnDiv.textContent = i;
-        columnDiv.setAttribute("style", "display: flex; flex-direction: column; flex: 1;")
-        containerDiv.appendChild(columnDiv)
-        for (let i = 0; i < row; i++) {
-            const rowDiv = document.createElement("div")
-            rowDiv.classList.add("square-divs")
-            // rowDiv.textContent = i
-            rowDiv.setAttribute("style", "display: flex; flex: 1; background-color: white; border: 1px solid black;")
-            columnDiv.appendChild(rowDiv)
-        }
+  //Create a lot of divs that change their height and width to fit the container perfectly, no matter how much of them
+  let gridSize = 20;
+  for (let i = 0; i < gridSize; i++) {
+    const columnDiv = document.createElement("div");
+    // columnDiv.textContent = i;
+    columnDiv.setAttribute(
+      "style",
+      "display: flex; flex-direction: column; flex: 1;"
+    );
+    containerDiv.appendChild(columnDiv);
+    for (let i = 0; i < gridSize; i++) {
+      const rowDiv = document.createElement("div");
+      rowDiv.classList.add("square-divs");
+      // rowDiv.textContent = i
+      rowDiv.setAttribute(
+        "style",
+        "display: flex; flex: 1; background-color: white; border: 1px solid black;"
+      );
+      columnDiv.appendChild(rowDiv);
     }
+  }
 }
-createGrids()
+createGrids();
 
-const rowDivHover = document.querySelectorAll(".square-divs")
+const rowDivHover = document.querySelectorAll(".square-divs");
 
 rowDivHover.forEach((div) => {
-    div.addEventListener("mouseenter", () => {
-        div.style.background = "black";
-    })
-})
+  div.addEventListener("mouseenter", () => {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    let bgColor = `rgb(${x}, ${y}, ${z})`
+    div.style.background = bgColor;
+  });
+});
+
+// const newGridBtn = document.querySelector(".new-grid");
+// newGridBtn.addEventListener("click", () => {
+//   let gridSize = prompt("How many squares do you want");
+//   createGrids(gridSize);
+// });
