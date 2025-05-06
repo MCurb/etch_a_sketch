83f6bff6b9a7855772e87b1e1d75e016ce738c21
MCurb <marcoscurbeco03@gmail.com>
2025-05-06 16:36:04 -0600

    Add an event listener to all child divs
    Now all child divs turn to black background color when on mouse enter

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const containerDiv = document.querySelector(".container")
 
 function createGrids() {
      //Create a lot of divs that change their height and width to fit the container perfectly, no matter how much of them
-     let row = 16;
-     let column = 16;
+     let row = 80;
+     let column = 80;
     for (let i = 0; i < column; i++) {
         const columnDiv = document.createElement("div")
         // columnDiv.textContent = i;
@@ -11,6 +11,7 @@ function createGrids() {
         containerDiv.appendChild(columnDiv)
         for (let i = 0; i < row; i++) {
             const rowDiv = document.createElement("div")
+            rowDiv.classList.add("square-divs")
             // rowDiv.textContent = i
             rowDiv.setAttribute("style", "display: flex; flex: 1; background-color: white; border: 1px solid black;")
             columnDiv.appendChild(rowDiv)
@@ -18,3 +19,11 @@ function createGrids() {
     }
 }
 createGrids()
+
+const rowDivHover = document.querySelectorAll(".square-divs")
+
+rowDivHover.forEach((div) => {
+    div.addEventListener("mouseenter", () => {
+        div.style.background = "black";
+    })
+})
