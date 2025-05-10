ece09825d950d5fc643b54e0327dfe91247cc398
MCurb <marcoscurbeco03@gmail.com>
2025-05-10 08:24:23 -0600

    Add eraser button to toggle erase/paint modes
    Toggle between paint and erase mode: clicking the eraser button now
    switches to erase mode (hover to remove div colors); clicking again
    returns to paint mode.

--- script.js
+++ script.js
@@ -13,10 +13,6 @@ function createGrids(gridSize) {
     for (let i = 0; i < gridSize; i++) {
       const rowDiv = document.createElement("div");
       rowDiv.classList.add("square-divs");
-      rowDiv.setAttribute(
-        "style",
-        "display: flex; flex: 1; background-color: rgb(216, 216, 216); opacity: 1; border: 0.1px solid gray;"
-      );
       columnDiv.appendChild(rowDiv);
     }
   }
@@ -30,9 +26,10 @@ containerDiv.addEventListener("mouseover", (e) => {
   }
 });
 
-//Every time one of the buttons is clicked, it removes the previous grid and calls the createGrids function again to create a new one
-const sizeButtonsDiv = document.querySelector(".size-buttons");
-sizeButtonsDiv.addEventListener("click", (e) => {
+//Event listener to the buttons div, that catches when any button is clicked
+const buttonsDiv = document.querySelector(".buttons");
+buttonsDiv.addEventListener("click", (e) => {
+  //If one of the grid size modifiers gets clicked, it will remove the previous grid and create a new one with a different size
   if (e.target.matches(".small-density")) {
     while (containerDiv.hasChildNodes()) containerDiv.firstChild.remove();
     createGrids(16);
@@ -43,23 +40,50 @@ sizeButtonsDiv.addEventListener("click", (e) => {
     while (containerDiv.hasChildNodes()) containerDiv.firstChild.remove();
     createGrids(80);
   } else if (e.target.matches(".custom-grid")) {
-    //Logic for the custom grid button and filters to the prompt answers
+    //Logic for the custom grid button that filters the prompt answers
     let gridNumber;
     while (true) {
       gridNumber = prompt(
         "How many grids do you want? Enter a number between 1-100"
       );
+      //If user cancels, end the loop
       if (gridNumber === null) {
-        //If user cancels, end the loop
         break;
       }
+      //Convert the answer to a number
       gridNumber = parseInt(gridNumber);
+      //If input is a number larger than 0 and smaller or equal to 100, remove all square divs and pass the number to the createGrids function, if not, ask again.
       if (!isNaN(gridNumber) && gridNumber > 0 && gridNumber <= 100) {
-        //If input is a number larger than 0 and smaller or equal to 100, remove all square divs and pass the number to the createGrids function, if not, ask again.
         while (containerDiv.hasChildNodes()) containerDiv.firstChild.remove();
         createGrids(gridNumber);
         break;
       }
     }
+    //Eraser Button logic
+  } else if (e.target.matches(".eraser-btn")) {
+    //If isErasing is false, set it to true and call the toggleBackground function through the event listener
+    if (isErasing === false) {
+      isErasing = true
+      //Change btn background to active color
+      eraserBtn.style.background = "hsl(53, 100%, 70%)"
+      containerDiv.addEventListener("mouseover", toggleBackground);
+    } else if (isErasing === true) {
+      isErasing = false
+      eraserBtn.style.background = "hsl(60, 100%, 96%)"
+      containerDiv.addEventListener("mouseover", toggleBackground);
+    }
+    
   }
 });
+
+const eraserBtn = document.querySelector(".eraser-btn")
+let isErasing = false
+
+//If isErasing true, change the targeted square div to white, if not give it a random color
+function toggleBackground(event) {
+  if (isErasing === true) {
+    event.target.style.background = "hsl(0, 0.00%, 84.70%)";
+  } else if (isErasing === false) {
+    event.target.style.background = `hsl(${Math.random() * 360}, 80%, 50%)`;
+  }
+}
